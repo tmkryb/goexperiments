efe909006071eb2c28fde3638429e6eb241cc219
tmkryb <tmkryb@gmail.com>
2019-08-01 22:02:51 +0200

    array and some cleanup

--- main.go
+++ main.go
@@ -21,13 +21,26 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func printSlice(w http.ResponseWriter, r *http.Request) {
-	elemStringVal := r.URL.Query().Get("elemNum")
-	log.Printf(elemStringVal)
-	elemNum, _ := strconv.Atoi(elemStringVal)
+	elemNum, err := getElemStringVal(r)
+	if err != nil {
+		log.Printf("The value equals: %v", elemNum)
+		slices := services.CreateSlice(elemNum)
+		json.NewEncoder(w).Encode(slices)
+	} else {
+		returnServerError(w, err, "Error on creating slice: %v")
+	}
 
+}
+
+func printArray(w http.ResponseWriter, r *http.Request) {
+	elemNum, err := getElemStringVal(r)
 	log.Printf("The value equals: %v", elemNum)
-	slices := services.CreateSlice(elemNum)
-	json.NewEncoder(w).Encode(slices)
+	array, err := services.CreateArray(elemNum)
+	if err != nil {
+		returnServerError(w, err, "Error on creating array: %v")
+	} else {
+		json.NewEncoder(w).Encode(array)
+	}
 }
 
 func about(w http.ResponseWriter, r *http.Request) {
@@ -45,6 +58,22 @@ func handleRequests() {
 	http.HandleFunc("/", homePage)
 	http.HandleFunc("/about", about)
 	http.HandleFunc("/slice", printSlice)
+	http.HandleFunc("/array", printArray)
 	http.HandleFunc("/users", getUsers)
 	http.ListenAndServe(":8081", nil)
+}
+
+//some helper functions for handling http
+func getElemStringVal(r *http.Request) (int, error) {
+	elemStringVal := r.URL.Query().Get("elemNum")
+	log.Printf(elemStringVal)
+	elemNum, err := strconv.Atoi(elemStringVal)
+	return elemNum, err
+
+}
+
+func returnServerError(w http.ResponseWriter, err error, message string) {
+	log.Printf(message, err)
+	w.WriteHeader(http.StatusInternalServerError)
+	fmt.Fprintf(w, message, err.Error())
 }
--- services/types.go
+++ services/types.go
@@ -1,9 +1,27 @@
 package services
 
+import (
+	"errors"
+)
+
+//Go exported functions always start with big letters!
+//Slices expand when appending new values to them
 func CreateSlice(elemNum int) []int {
 	intSlice := make([]int, 0, 10)
 	for i := 0; i < elemNum; i++ {
 		intSlice = append(intSlice, i)
 	}
 	return intSlice
+}
+
+//Array is always of fixed size
+func CreateArray(elemNum int) ([10]int, error) {
+	intArray := [10]int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	if elemNum > 10 {
+		return intArray, errors.New("elemNum is to big! limit it to 10")
+	}
+	for i := 0; i < elemNum; i++ {
+		intArray[i] = i
+	}
+	return intArray, nil
 }
